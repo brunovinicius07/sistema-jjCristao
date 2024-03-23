d3920ed3229a9381f311bcfea559cf373513f01d
Bruno Vinicius <brunoviniciusoliveiratavares@gmail.com>
2024-03-22 23:17:08 -0300

    refactored code

---
+++ pages/venda.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="icon" type="imagem/png" href="../logo/jj-colorido-2.png"/>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Venda</title>
+</head>
+<body style="background-color: #F6F3EA;">
+<header>
+    <div class="header">
+        <div class="logo">
+        <div class="logoimg"><a href=""><img src="../logo/jj-colorido-1.png" alt=""></a></div>
+        </div>
+        <div class="menu">
+            <img class="menu-opener" src="assets/images/menu.png">
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="cadastro.php">Cadastrar Produto</a></li>
+                    <li><a href="pesquisa.php">Estoque</a></li>
+                    <li><a href="movimentacao.php">Entrada / Saída</a></li>
+                    <li class="active"><a href="venda.php">Venda</a></li>
+                    <li><a href="financeiro.php">Financeiro</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<?php     
+    $pesquisa = $_POST['busca'] ?? '';
+
+    include "../connection/connection.php";
+
+    $sql = "SELECT * FROM product WHERE nome LIKE '%$pesquisa%'";
+
+    $dados = mysqli_query($conn, $sql)
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Pesquisar</h1>
+            <nav style="background-color: #F6F3EA;" >
+                <form class="form-inline" action="pesquisa.php" method="POST">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Produto" aria-label="Search" name="busca" autofocus>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+                </form>
+            </nav>
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Imagem</th>
+                        <th>Nome</th>
+                        <th>Material</th>
+                        <th>Tamanho</th>
+                        <th>Valor Compra</th>
+                        <th>Valor Venda</th>
+                        <th>Quantidade</th>
+                        <th>Funções</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        while ($linha = mysqli_fetch_assoc($dados)) {
+                            $idProduto = $linha['idProduto']; 
+                            $nome = $linha['nome']; 
+                            $material = $linha['material']; 
+                            $tamanho = $linha['tamanho']; 
+                            $valorCompra = $linha['valorCompra']; 
+                            $valorVenda = $linha['valorVenda']; 
+                            $quantidade = $linha['quantidade']; 
+                            $imagem = $linha['imagem'];
+
+                            echo "<tr>
+                                    <th><img src='../img/$imagem' class='lista_img'></th>
+                                    <th scope='row'>$nome</th>
+                                    <td>$material</td>
+                                    <td>$tamanho cm</td>
+                                    <td>R$ $valorCompra</td>
+                                    <td>R$ $valorVenda</td>
+                                    <td>$quantidade uni</td>
+                                    <td width=180px>
+                                        
+                                    <button id='btn_$idProduto' class='btn btn-info' data-toggle='modal' data-target='#confirma' onclick='selecionarParaVenda($idProduto, \"$nome\", \"$imagem\")'>Selecionar Para Venda</button>
+
+                                    </td>
+                                </tr>";
+                        }
+                    ?>
+                    
+                    <script type="text/javascript">
+                        // Lista para armazenar os IDs dos produtos selecionados
+                        var produtosSelecionados = [];
+
+                        function selecionarParaVenda(id, nome, imagemUrl) {
+                            var botao = document.getElementById('btn_' + id);
+                            if (botao.innerHTML === 'Selecionado') {
+                                // Remover o ID do produto da lista de selecionados
+                                var index = produtosSelecionados.indexOf(id);
+                                if (index !== -1) {
+                                    produtosSelecionados.splice(index, 1);
+                                }
+                                botao.innerHTML = 'Selecionar Para Venda';
+                                botao.classList.remove('btn-sucess');
+                                botao.classList.add('btn-info');
+                            } else {
+                                // Adicionar o ID do produto à lista de selecionados
+                                produtosSelecionados.push(id);
+                                botao.innerHTML = 'Selecionado';
+                                botao.classList.remove('btn-info');
+                                botao.classList.add('btn-success');
+                                obterDados(id, nome, imagemUrl);
+                            }
+                        }
+
+                        function redirecionarParaVenda() {
+                            if (produtosSelecionados.length > 0) {
+                                // Construir a URL da página de confirmação de venda com os IDs dos produtos selecion
+                                var url = 'venda_confirma.php?produtos=' + produtosSelecionados.join(',');
+                                // Redirecionar para a página de confirmação de venda
+                                window.location.href = url;
+                            } else {
+                                alert('Nenhum produto selecionado. \n \n Selecione ao menos um produto para venda!');
+                            }
+                        }
+                    </script>
+
+                </tbody>
+            </table>
+
+            <a href="index.php" class="btn btn-info">Voltar</a>
+            <button onclick="redirecionarParaVenda()" class="btn btn-primary">Vender Produtos Selecionados</button>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    function obterDados(id, nome, imagemUrl){
+        document.getElementById('nome').innerHTML = nome;
+        document.getElementById('nome_produto').value = nome;
+        document.getElementById('idProduto').value = id;
+        document.getElementById('imagem_produto').innerHTML = "<img src='../img/" + imagemUrl + "' class='mostrar_image'>";
+    }
+</script>
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ pages/venda_confirma.php
@@ -0,0 +1,151 @@
+<?php
+if (isset($_GET['produtos'])) {
+    $produtosSelecionados = $_GET['produtos'];
+    $idsProdutos = explode(',', $produtosSelecionados);
+    include "../connection/connection.php";
+
+    $sql = "SELECT * FROM product WHERE idProduto IN (" . implode(',', $idsProdutos) . ")";
+    $dados = mysqli_query($conn, $sql);
+
+    
+} else {
+    header("Location: pesquisa.php");
+    exit();
+}
+?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="icon" type="imagem/png" href="../logo/jj-colorido-2.png"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Confirmação de Venda</title>
+</head>
+<body style="background-color: #F6F3EA;">
+<header>
+    <div class="header">
+        <div class="logo">
+            <div class="logoimg"><a href=""><img src="../logo/jj-colorido-1.png" alt=""></a></div>
+        </div>
+        <div class="menu">
+            <img class="menu-opener" src="assets/images/menu.png">
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="cadastro.php">Cadastrar Produto</a></li>
+                    <li><a href="pesquisa.php">Estoque</a></li>
+                    <li><a href="movimentacao.php">Entrada / Saída</a></li>
+                    <li class="active"><a href="venda.php">Venda</a></li>
+                    <li><a href="financeiro.php">Financeiro</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col">
+            <h1 class="mb-4">Confirmação de Venda</h1>
+
+            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="row">
+                    <?php
+                    while ($linha = mysqli_fetch_assoc($dados)) {
+                        $idProduto = $linha['idProduto'];
+                        $nome = $linha['nome'];
+                        $valorCompra = $linha['valorCompra'];
+                        $valorVenda = $linha['valorVenda'];
+                        $imagem = $linha['imagem'];
+                    ?>
+                        <div class="col-md-4 mb-3">
+                            <div class="card">
+                                <img src="../img/<?php echo $imagem; ?>" class="card-img-top" alt="Imagem do Produto">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $nome; ?></h5>
+                                    <p class="card-text">Preço de Compra: R$ <?php echo $valorCompra; ?></p>
+                                    <p class="card-text">Preço de Venda: R$ <?php echo $valorVenda; ?></p>
+                                    <div class="form-group">
+                                        <label for="quantidade_<?php echo $idProduto; ?>">Quantidade:</label>
+                                        <input type="number" class="form-control" id="quantidade_<?php echo $idProduto; ?>" name="quantidade_<?php echo $idProduto; ?>" value="1" min="1">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Teve Desconto?</h5>
+                                <div class="form-group">
+                                    <label>Informe se houve desconto:</label>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="desconto_global" id="desconto_sim_global" value="sim" onclick="toggleDescontoGlobal()">
+                                        <label class="form-check-label" for="desconto_sim_global">Sim</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="desconto_global" id="desconto_nao_global" value="nao" onclick="toggleDescontoGlobal()" checked>
+                                        <label class="form-check-label" for="desconto_nao_global">Não</label>
+                                    </div>
+                                </div>
+                                <div class="form-group" id="campo_desconto_global" style="display: none;">
+                                    <label for="valor_desconto_global">Valor do Desconto:</label>
+                                    <input type="number" class="form-control" id="valor_desconto_global" name="valor_desconto_global" placeholder="R$:">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#confirmarVendaModal">Confirmar Venda</button>
+            </form>
+
+            <a href="venda.php" class="btn btn-info mt-3">Voltar</a>
+        </div>
+    </div>
+</div>
+
+<!-- Modal de confirmação de venda -->
+<div class="modal fade" id="confirmarVendaModal" tabindex="-1" role="dialog" aria-labelledby="confirmarVendaModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmarVendaModalLabel">Venda Realizada</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Produtos vendidos:</p>
+                <ul>
+                    
+                </ul>
+                <p>Valor Total da Venda: R$ <?php $valorTotal ?> </p>
+            </div>
+            <div class="modal-footer">
+                <a href="venda.php" class="btn btn-success">Voltar</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function toggleDescontoGlobal() {
+        var campoDesconto = document.getElementById('campo_desconto_global');
+        var descontoCheckbox = document.getElementById('desconto_sim_global');
+        campoDesconto.style.display = descontoCheckbox.checked ? 'block' : 'none';
+    }
+
+    toggleDescontoGlobal();
+</script>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
